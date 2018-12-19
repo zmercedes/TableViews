15726f9c6abb5fce76dcebe025541922c3357643
zmercedes <zmercedes89@gmail.com>
2018-12-19 09:54:40 -0500

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Zoilo Mercedes on 12/19/18.
+//  Copyright © 2018 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
