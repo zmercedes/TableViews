a97ede9887e987bacb4fb8ca849b99b7a48fc657
zmercedes <zmercedes89@gmail.com>
2018-12-22 10:01:15 -0500

    added an alert and a sharing button

--- TableViewPractice/ViewController.swift
+++ TableViewPractice/ViewController.swift
@@ -22,5 +22,21 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         textView.text = text
     }
+    
+    @IBAction func ShowAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Button Was Pressed",message: "This is " + text, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "ok", style: .cancel) { (action) in
+            print("pressed!")
+        }
+        alert.addAction(action1)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func ShareToApp(_ sender: Any) {
+        let activityViewController = UIActivityViewController(activityItems: [textView.text], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    
 }
 
